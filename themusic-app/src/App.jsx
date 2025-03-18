5fa1af14d0397a28e59982f32b4b85b04a8c3fb8
Aleksi <aleksi.kaurila@gmail.com>
2025-03-18 14:46:46 +0200

    Initialized the project with tailwind

---
+++ themusic-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+
+  return (
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white
+    text-sm sm:text-base'>
+
+    </main>
+  )
+}
+
+export default App
