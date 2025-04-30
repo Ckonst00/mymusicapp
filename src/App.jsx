7317157d293b4d3074904cbd571395245c050364
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-04-30 18:02:57 +0300

    Added a video bg and changed some text format

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import AudioPlayer from "./Components/Player/AudioPlayer.jsx";
+
+
 function App() {
 
-
-  return (
-    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white
-    text-sm sm:text-base'>
-      <div>
-        <AudioPlayer />
-      </div>
-    </main>
-  )
+    return (
+        <div className='app-container'>
+            {/* Background video */}
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className='video-bg'
+            >
+                <source src="/StyleContent/space-meteor-bg.mp4" type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
+                <div className="video-overlay"></div>
+            {/* Foreground content */}
+            <main className='main'>
+                <div>
+                    <AudioPlayer/>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App
--- src/Components/Player/AudioPlayer.jsx
+++ src/Components/Player/AudioPlayer.jsx
@@ -5,7 +5,7 @@ function AudioPlayer() {
 
     return (
         <figure className=" flex flex-col items-center justify-center h-screen space-y-4">
-            <figcaption className="text-lg font-semibold">Click Play to listen:</figcaption>
+            <figcaption className="text-xl font-semibold text-orange-400">Click Play to listen</figcaption>
             <audio controls src="src/SongsTemp/Entrance-of the-Gladiators.mp3"></audio>
         </figure>
 
