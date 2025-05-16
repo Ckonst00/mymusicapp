bfb32325a90734732b8206da2ca510ace1595f4e
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-05-16 13:14:21 +0300

    let me push ffs!

--- src/Components/Player/AudioPlayer.jsx
+++ src/Components/Player/AudioPlayer.jsx
@@ -4,9 +4,12 @@ function AudioPlayer() {
 
 
     return (
-        <figure className=" flex flex-col items-center justify-center h-screen space-y-4">
+        <figure className=" flex flex-col items-center justify-end h-screen space-y-4">
             <figcaption className="text-xl font-semibold text-orange-400">Click Play to listen</figcaption>
             <audio controls src="src/SongsTemp/Entrance-of the-Gladiators.mp3"></audio>
+            <button type="button" className="w-12 h-12 rounded-full bg-orange-400 text-white shadow-lg hover:bg-orange-500 transition">
+                Play
+            </button>
         </figure>
 
     )
